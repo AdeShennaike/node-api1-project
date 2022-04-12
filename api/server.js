7f24af7964d3da230885b83242c6a0d75f9f0fbd
Ade <shennaike2386@hotmail.com>
2022-04-12 01:24:22 -0400

    get endpoint done

--- api/server.js
+++ api/server.js
@@ -1,5 +1,16 @@
 // BUILD YOUR SERVER HERE
+const Users = require('./users/model')
+
 const express = require('express')
 const server = express()
 
+server.get( "/api/users", (req, res) => {
+    Users.find()
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(err => {
+        res.status(500).json({ message: "The users information could not be retrieved" })
+    })
+})
 module.exports = server; // EXPORT YOUR SERVER instead of {}
