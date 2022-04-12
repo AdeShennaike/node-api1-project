2e0db7caa8a197f5f9fe053fed12f66ef377235d
Ade <shennaike2386@hotmail.com>
2022-04-12 02:52:12 -0400

    put endpoint done

--- api/server.js
+++ api/server.js
@@ -56,6 +56,24 @@ server.post("/api/users", async (req, res) => {
 })
 
 server.put("/api/users/:id", (req, res) => {
+    const {name, bio} = req.body
+    const changes = {name: name, bio: bio}
+    const {id} = req.params
 
+    if (!name || !bio) {
+        res.status(400).json({ message: "Please provide name and bio for the user" })
+    }else {
+        Users.update(id, changes)
+            .then(user => {
+                if(!user){
+                    res.status(404).json({ message: "The user with the specified ID does not exist" })
+                }else{
+                    res.status(200).json(user)
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ message: "The user information could not be modified" })
+            })
+    }
 })
 module.exports = server; // EXPORT YOUR SERVER instead of {}
