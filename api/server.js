c5f1629a304d83722c773a692391809d9f2932de
Ade <shennaike2386@hotmail.com>
2022-04-12 01:33:38 -0400

    get by ID  endpoint done

--- api/server.js
+++ api/server.js
@@ -4,13 +4,26 @@ const Users = require('./users/model')
 const express = require('express')
 const server = express()
 
-server.get( "/api/users", (req, res) => {
+server.get("/api/users", (req, res) => {
     Users.find()
-    .then(user => {
-        res.status(200).json(user)
-    })
-    .catch(err => {
-        res.status(500).json({ message: "The users information could not be retrieved" })
-    })
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "The users information could not be retrieved" })
+        })
+})
+
+server.get( "/api/users/:id", async ( req, res ) => {
+    const userById = await Users.findById(req.params.id)
+    
+    try{ if(!userById){
+        res.status(404).json({ message: "The user with the specified ID does not exist" })
+    }else{
+        res.status(200).json(userById)
+    }
+    }catch(err){
+        res.status(500).json({ message: "The user information could not be retrieved" })
+    }    
 })
 module.exports = server; // EXPORT YOUR SERVER instead of {}
