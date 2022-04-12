d75e3563f5677362970d86cdfe59c5869fbe4b75
Ade <shennaike2386@hotmail.com>
2022-04-12 02:01:37 -0400

    delete  endpoint done

--- api/server.js
+++ api/server.js
@@ -26,4 +26,15 @@ server.get( "/api/users/:id", async ( req, res ) => {
         res.status(500).json({ message: "The user information could not be retrieved" })
     }    
 })
+
+server.delete("/api/users/:id", (req, res) => {
+    Users.remove(req.params.id)
+        .then(user => {
+            if(!user){res.status(404).json({ message: "The user with the specified ID does not exist" })}
+            else{res.status(200).json(user)}
+        })
+        .catch(err => {
+            res.status(500).json({ message: "The user could not be removed" })
+        })
+})
 module.exports = server; // EXPORT YOUR SERVER instead of {}
